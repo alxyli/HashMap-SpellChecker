ff38b66c8e813543385c8d9062a9da3ea5e95f7a
Alex Li <alex@Alexs-MacBook-Pro.local>
2018-07-07 03:16:43 -0700

    finished rewriting HashMap class in C++

--- hashMap.hpp
+++ hashMap.hpp
@@ -27,7 +27,7 @@ public:
 		mSize = 0;
 		mTable = new HashLink<K, V> *[capacity]();
 		for (int i = 0; i < capacity; i++)		
-			mTable[i] = NULL;
+			mTable[i] = nullptr;
 	}
 
 	/*
@@ -40,7 +40,7 @@ public:
 		// free all links in each bucket
 		for (int i = 0; i < mCapacity; i++) {
 			temp = mTable[i];
-			while (temp != NULL) {
+			while (temp != nullptr) {
 				next = temp->getNext();
 				delete temp;
 				temp = next;
@@ -50,10 +50,10 @@ public:
 	}
 
 	/*
-	 * Returns a pointer to the value of the link with the given key. Returns NULL if no 
+	 * Returns a pointer to the value of the link with the given key. Returns nullptr if no 
 	 * link with the given key is in the table.
 	 * @param key 
-	 * @return link value or NULL 
+	 * @return link value or nullptr 
 	 */
 	V* mapGet(const K &key) {
 		// get index of bucket
@@ -62,7 +62,7 @@ public:
 			index += mapCapacity();
 
 		HashLink<K, V> *temp = mTable[index];
-		while (temp != NULL) {
+		while (temp != nullptr) {
 			if (temp->getKey().compare(key) == 0) {
 				V tempVal = temp->getValue();
 				return &tempVal;
@@ -70,7 +70,7 @@ public:
 
 			temp = temp->getNext();
 		}
-		return NULL;
+		return nullptr;
 	}
 
 	/*
@@ -106,7 +106,7 @@ public:
 			else {
 				// add new link to end of bucket
 				HashLink<K, V> *temp = mTable[index];
-				while (temp->getNext() != NULL) 
+				while (temp->getNext() != nullptr) 
 					temp = temp->getNext();
 
 				temp->setNext(newLink);
@@ -115,15 +115,54 @@ public:
 		}
 	}
 
-	void mapRemove(const K &key);
+	/*
+	 * Attemps to remove the key-value pair specified by the key parameter from the hash table.
+	 * Returns true if removal was successful and false otherwise
+	 * @param key 
+	 * @return bool indicating whether key-value pair removal was successful
+	 */
+	bool mapRemove(const K &key) {
+		if (mapContains(key)) {
+			int index = HASH_FUNCTION(key) % mapCapacity();
+			if (index < 0)
+				index += mCapacity;
 
+			HashLink<K, V> *temp = mTable[index];
+			HashLink<K, V> *prev = nullptr;
+			while (temp != nullptr) {
+				if (temp->getKey().compare(key) == 0) {
+					if (prev)
+						prev->setNext(temp->getNext());
+
+					mTable[index] = temp->getNext();
+					delete temp;
+					mSize--;
+					return true;
+				}
+
+				else {
+					prev = temp;
+					temp = temp->getNext();
+				}
+			}
+		}
+
+		return false;
+	}
+
+	/*
+	 * Attemps to locate the key-value pair specified by the key parameter from the hash table.
+	 * Returns true if the key-value pair is found in the table and false otherwise
+	 * @param key 
+	 * @return bool indicating whether key exists in table
+	 */
 	bool mapContains(const K &key) {
 		int index = HASH_FUNCTION(key) % mapCapacity();
 		if (index < 0)
 			index += mCapacity;
 
 		HashLink<K, V> *temp = mTable[index];
-		while (temp != NULL) {
+		while (temp != nullptr) {
 			if (temp->getKey().compare(key) == 0)
 				return true;
 
@@ -133,31 +172,55 @@ public:
 		return false;
 	}
 
+	/*
+	 * Returns the number of links in the hash table
+	 * @return number of links
+	 */
 	int mapSize() const { return mSize; }
+
+	/*
+	 * Returns number of buckets in the hash table
+	 * @return number of buckets
+	 */
 	int mapCapacity() const { return mCapacity; } 
+
+	/*
+	 * Returns the number of table buckets without any links
+	 * @return numbeer of empty buckets
+	 */
 	int mapEmptyBuckets() const {
 		int empty = 0;
 		for (int i = 0; i < mapCapacity(); i++) {
-			if (mTable[i] == NULL)
+			if (mTable[i] == nullptr)
 				empty++;
 		}
 
 		return empty;
 	}
 
+	/*
+	 * Returns the ratio of (links / buckets) in the table currently
+	 * @return map table load
+	 */
 	double mapTableLoad() const {
 		double links = (double)mapSize();
 		double buckets = (double)mapCapacity();
 		return (links / buckets);
 	}
 
-	void hashMapCleanup(HashLink<K, V> **map) {
+	/*
+	 * Removes all links in the table and frees allocated memory. Used as a helper function
+	 * in the resizeTable() method when the table load exceeds MAP_TABLE_LOAD threshold
+	 * @param hash table to be cleaned
+	 * @param capacity of table
+	 */
+	void hashMapCleanup(HashLink<K, V> **map, int capacity) {
 		HashLink<K, V> *temp, *next;
 
 		// free link pointers in each bucket
-		for (int i = 0; i < mCapacity; i++) {
+		for (int i = 0; i < capacity; i++) {
 			temp = map[i];
-			while (temp != NULL) {
+			while (temp != nullptr) {
 				next = temp->getNext();
 				delete temp;
 				temp = next;
@@ -165,23 +228,31 @@ public:
 		}
 	}
 
+	/*
+	 * Resizes the hash table to contain newCapacity number of buckets. After the new
+	 * table is allocated, all old links are re-hashed into the new table. The old table
+	 * memory is then deallocated, including all of the old links
+	 * @param new capacity (number of buckets)
+	 */
 	void resizeTable(int newCapacity) {
 		// keep reference to old table for copy and delete
 		HashLink<K, V> **oldTable = mTable;
+		int oldCapacity = mCapacity;
+		mCapacity = newCapacity;
 
 		// allocate new hash map with updated capacity
 		mTable = new HashLink<K, V> *[newCapacity]();
 
 		for (int i = 0; i < newCapacity; i++)		
-			mTable[i] = NULL;
+			mTable[i] = nullptr;
 
 		// reset size counter and update new size with mapPut()
 		mSize = 0;
-		for (int i = 0; i < mCapacity; i++) {
+		for (int i = 0; i < oldCapacity; i++) {
 			HashLink<K, V> *temp = oldTable[i];
 
 			// rehash links from old table into new table
-			while (temp != NULL) {
+			while (temp != nullptr) {
 				K tempKey = temp->getKey();
 				V tempVal = temp->getValue();
 				mapPut(tempKey, tempVal);
@@ -190,8 +261,7 @@ public:
 		}
 
 		// delete links from old table, set new table capacity, and free the old table
-		hashMapCleanup(oldTable);
-		mCapacity = newCapacity;
+		hashMapCleanup(oldTable, oldCapacity);
 		delete[] oldTable;
 	}
 
@@ -205,13 +275,13 @@ public:
 	friend std::ostream& operator<<(std::ostream& os, const HashMap<K, V>& map) {
 		for (int i = 0; i < map.mapCapacity(); i++) {
 			HashLink<K, V> *entry = map.mTable[i];
-			if (entry != NULL) {
+			if (entry != nullptr) {
 				os << "Bucket " << i << " -> ";
-				while (entry != NULL) {
+				while (entry != nullptr) {
 					os << "(" << entry->getKey() << ", " << entry->getValue() << ") -> ";
 					entry = entry->getNext();
 				}
-				os << "null";
+				os << "nullptr";
 				os << endl;
 			}
 		}
@@ -219,6 +289,10 @@ public:
 		return os;
 	}
 
+	/*
+	 * Hashes the key by folding (summing) each character
+	 * @returns hashed value for input key
+	 */
 	int hashFunction1(const K &key) {
 		int r = 0;
 		for (int i = 0; key[i] != '\0'; i++)
@@ -227,6 +301,12 @@ public:
 		return r;
 	}
 
+	/*
+	 * Hashes the key by shifting the value of each character, then folding (summing)
+	 * hashFunction2 prevents anagrams hashing to the same value (via the shift), thus
+	 * resulting in fewer collisions
+	 * @returns hashed value for input key
+	 */
 	int hashFunction2(const K &key) {
 		int r = 0;
 		for (int i = 0; key[i] != '\0'; i++)
